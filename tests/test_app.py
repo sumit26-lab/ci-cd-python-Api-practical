69e3c680f3d097070c661a4e3fa86dca6e007bdd
sumit26-lab <sr26rajput@gmail.com>
2026-04-07 12:08:49 +0530

    this my initi commit

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello DevOps" in response.data
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
+
+
