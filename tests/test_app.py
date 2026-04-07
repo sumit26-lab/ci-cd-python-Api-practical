3deab0e85da798e81dfd916f6c36039a2f89d7bb
sumit26-lab <sr26rajput@gmail.com>
2026-04-07 16:09:09 +0530

    add  test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,12 +5,13 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello DevOps" in response.data
+    assert "Hello DevOps" in response.data
+
 
 
 def test_health():
     client = app.test_client()
     response = client.get("/health")
-    
+    assert response.status_code == 200
 
 
