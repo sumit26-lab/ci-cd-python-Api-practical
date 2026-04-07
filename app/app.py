a9c064dcbb67aafb3a1b97421f57eeee473b4857
sumit26-lab <sr26rajput@gmail.com>
2026-04-07 16:06:16 +0530

    add  test_app.py

--- app/app.py
+++ app/app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 def home():
     return jsonify(message="Hello DevOps Engg")
 
+
 @app.route("/health")
 def health():
     return jsonify(status="OK")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,16 @@
 from app.app import app
+
+
 def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert b"Hello DevOps" in response.data
 
+
 def test_health():
     client = app.test_client()
     response = client.get("/health")
-    assert response.status_code == 200
+    
 
 
