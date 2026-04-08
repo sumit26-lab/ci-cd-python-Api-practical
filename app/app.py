9015d42b1f9efbf7d9326d188e22509f0791d665
sumit26-lab <sr26rajput@gmail.com>
2026-04-08 21:32:21 +0530

    remove space in app.py

--- app/app.py
+++ app/app.py
@@ -1,17 +1,23 @@
 from flask import Flask, jsonify, render_template, request
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
+    # Return JSON for API/testing
     if request.headers.get("Accept") == "application/json":
         return jsonify(message="Hello DevOps Engg")
 
-    return render_template("index.html")   # ✅ FIXED
+    # Return HTML for browser
+    return render_template("index.html")
+
 
 @app.route("/health")
 def health():
     return jsonify(status="OK")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
