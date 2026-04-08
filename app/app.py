6e8553aa10bdbdd98ba574b9bda2813fdbca00f2
sumit26-lab <sr26rajput@gmail.com>
2026-04-08 16:38:21 +0530

    have need  blank space Error Resolve by sumit

--- app/app.py
+++ app/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify, send_file
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return send_file("../index.html")   # simple direct file serve
 
+
 @app.route("/health")
 def health():
     return jsonify(status="OK")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
