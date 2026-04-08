de868b22c9917c253d92436e6ff8ab29cbea8cf7
sumit26-lab <sr26rajput@gmail.com>
2026-04-08 21:29:54 +0530

    change template for image use

--- app/app.py
+++ app/app.py
@@ -1,22 +1,17 @@
-from flask import Flask, jsonify, send_file, request
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    # Return JSON for API/testing
     if request.headers.get("Accept") == "application/json":
         return jsonify(message="Hello DevOps Engg")
 
-    # Return HTML for browser
-    return send_file("../index.html")
-
+    return render_template("index.html")   # ✅ FIXED
 
 @app.route("/health")
 def health():
     return jsonify(status="OK")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
