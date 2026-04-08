5f3fed41c28b4ea00231f045e44a0e190d7458f4
sumit26-lab <sr26rajput@gmail.com>
2026-04-08 16:59:49 +0530

    fix the test case issue

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ from app.app import app
 def test_home():
     client = app.test_client()
     response = client.get("/", headers={"Accept": "application/json"})
-    
+
     assert response.status_code == 200
 
     data = response.get_json()
